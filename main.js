6380f6edb5ed8f4fa7c8408bcaeb2c2271e90e19
3jun <34735492+kk3june@users.noreply.github.com>
2020-12-27 13:03:20 +0900

    1st commit

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 const input = document.querySelector('.input__swing');
+const match = document.getElementById('match');
 
 function swing() {
     let numbers = input.value;
     let chkNum = isNaN(numbers);
-    let chkNumCount = numbers.toString.length;
+    let chkNumCount = numbers.toString().length;
 
     if( chkNum === true) {
         alert('숫자만 입력하세요.')
     } else {
         console.log(chkNumCount);
+        if( chkNumCount === 3) {
+            play(numbers);
+        } else {
+            alert('세자리 숫자만 입력 가능합니다.');
+        }
     }
+}
 
-    console.log(chkNum);
+function play(num) {
+    console.log('내가 입력한 숫자' + num);
+    let matchDiv = document.createElement('div');
+    let matchSpan = document.createElement('span');
+
+    matchDiv.classList.add('match-line');
+
+    match.appendChild(matchDiv).appendChild(matchSpan).classList.add('match__inning');
+
 }
 
 input.addEventListener('keypress', (e)=> {
